d572ec454963681a8b74c3a6090b34df3c2602c4
Robert Vari <mail.robertvari@gmail.com>
2023-01-21 14:40:28 +0100

    update

---
+++ 03_multiple_positional_arguments.py
@@ -0,0 +1,6 @@
+def say_hello(name, email, address):
+    print(f"Hello {name}")
+    print(f"Your email: {email}")
+    print(f"Your address:  {address}")
+
+say_hello("csaba@gmail.com", "Csaba", "Budapest")
