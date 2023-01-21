413af4d66b9b5cc0e6a8a0dcc4da7ba42ce3f87b
Robert Vari <mail.robertvari@gmail.com>
2023-01-21 16:43:27 +0100

    update

---
+++ 14_using_libraries.py
@@ -0,0 +1,8 @@
+# import my_math_functions and give an alias
+from utilities import my_math_functions as mf
+
+# import individual functions
+from utilities.my_math_functions import add_numbers, divide_numbers
+
+from os import listdir
+import random
---
+++ utilities/my_math_functions.py
@@ -0,0 +1,10 @@
+def add_numbers(a: int, b: int):
+    return a + b
+
+def multiply_numbers(a: int, b: int):
+    "Multipy two numbers"
+    return a * b
+
+def divide_numbers(a: int, b:int):
+    "Divide two numbers"
+    return a / b
