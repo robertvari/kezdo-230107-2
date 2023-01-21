5ed401e36a621c31118900c0636078c92750bd6b
Robert Vari <mail.robertvari@gmail.com>
2023-01-21 15:03:57 +0100

    update

---
+++ 06_variable_length_arguments.py
@@ -0,0 +1,14 @@
+def print_all_params(*args):
+    for i in args:
+        print(i)
+
+print_all_params(
+    "Robert", 
+    "Csilla", 
+    "robert@gmail.com", 
+    3.14,
+    "Budapest",
+    "Győr",
+    "Hétfő",
+    "stb..."
+)
