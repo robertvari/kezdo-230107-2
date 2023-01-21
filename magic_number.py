6708356c6f6fd9988011f79c27b87b11df412363
Robert Vari <mail.robertvari@gmail.com>
2023-01-21 17:08:52 +0100

    update

---
+++ magic_number.py
@@ -0,0 +1,17 @@
+REWARDS = {
+    "Chocolate bar": 10,
+    "Cinema ticker": 30,
+    "Travel ticker": 40
+}
+
+MIN = 1
+MAX = 10
+
+# player variables
+LIFES = 3
+CREDITS = 0
+PLAYER_NAME = None
+
+# entry point
+def main():
+    pass
