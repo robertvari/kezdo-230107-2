c17e71fee0e2aa1c33a39b54a6ad233a1bf7fca2
Robert Vari <mail.robertvari@gmail.com>
2023-01-21 17:32:35 +0100

    update

--- magic_number.py
+++ magic_number.py
@@ -1,3 +1,5 @@
+import os
+
 REWARDS = {
     "Chocolate bar": 10,
     "Cinema ticker": 30,
@@ -14,4 +16,34 @@ PLAYER_NAME = None
 
 # entry point
 def main():
+    # Start intro
+    intro()
+
+    # get player name and store it in PLAYER_NAME variable
+    get_player_name()
+
+    # start game
+    game_loop()
+
+def game_loop():
     pass
+
+def get_player_name():
+    global PLAYER_NAME
+    PLAYER_NAME = input("What is your name?")
+
+def intro():
+    clear_screen()
+    print("="*50, "MAGIC NUMBER", "="*50)
+    print("In this game you have to guess a number I'm thinking of. The number must be between 1 and 10.")
+
+def outro():
+    clear_screen()
+    print("See you later!")
+    exit()
+
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+main()
