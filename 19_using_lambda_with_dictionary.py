6c09e0537e6463353b456d8f84f0e0b65de03a77
Robert Vari <mail.robertvari@gmail.com>
2023-01-22 14:33:41 +0100

    update

---
+++ 19_using_lambda_with_dictionary.py
@@ -0,0 +1,6 @@
+movie_list = {
+    1: {"name": "The Princess", "rating": 63},
+    2: {"name": "Spider-Man: No Way Home", "rating": 81},
+    3: {"name": "Morbius", "rating": 64},
+    4: {"name": "The Northman", "rating": 72},
+}
