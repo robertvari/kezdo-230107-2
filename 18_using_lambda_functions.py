9628ea0e86ad67713941d39afee2cabe697cc3fe
Robert Vari <mail.robertvari@gmail.com>
2023-01-22 14:28:03 +0100

    update

--- 18_using_lambda_functions.py
+++ 18_using_lambda_functions.py
@@ -1,1 +1,6 @@
 name_list = ["Vári Róbert", "Kiss Elemér", "Nagy Adrienn", "Tóth Barna", "Kiss Elemér Aladár"]
+
+def split_name(name):
+    return name.split()[-1]
+
+print(sorted(name_list, key=split_name))
