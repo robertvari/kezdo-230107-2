822807f8fbfe597376fc1fa9c0993ad9b2c31b4b
Robert Vari <mail.robertvari@gmail.com>
2023-01-22 15:23:54 +0100

    update

--- 21_decorators.py
+++ 21_decorators.py
@@ -1,5 +1,14 @@
 import time, random
 
+def my_timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        print(f"Process time: {time.time() - start}")
+
+        return result
+    return wrapper
+
 def worker1():
     print("Worker 1 started...")
     time.sleep(random.randint(1, 10))
@@ -16,9 +25,6 @@ def worker3():
     print("Worker 3 finished!")
 
 
-
-
-
-start = time.time()
-worker()
-print(f"Process time: {time.time() - start}")
+worker1()
+worker2()
+worker3()
