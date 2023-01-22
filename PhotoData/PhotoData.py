440351c926480c238edf05228ee6150b91c7c2b1
Robert Vari <mail.robertvari@gmail.com>
2023-01-22 17:22:14 +0100

    update

---
+++ PhotoData/PhotoData.py
@@ -0,0 +1,22 @@
+import os, json
+from PIL import Image, ExifTags
+
+def main():
+    pass
+
+def get_folder_path():
+    pass
+
+def check_folder_path():
+    pass
+
+def get_photos():
+    pass
+
+def create_data():
+    pass
+
+def save_data():
+    pass
+
+main()
